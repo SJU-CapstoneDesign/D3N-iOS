11e5e3137a71fba746ebc510e7128b32f141c02b
Younghoon Ahn <ahnqwerty@gmail.com>
2023-09-29 21:58:17 +0900

    Update QuestionResultView.swift

--- Targets/D3N/Sources/Presentation/Question/Result/View/QuestionResultView.swift
+++ Targets/D3N/Sources/Presentation/Question/Result/View/QuestionResultView.swift
@@ -27,31 +27,57 @@ public struct QuestionResultView: View {
 //                    Spacer()
 //                }
 // TODO: navigation bar ROOT APP 단위에서 만들어 불러오기
-                ForEach(viewModel.quizs, id: \.self) { quiz in
-                    VStack (alignment: .trailing){
-                        HStack {
-                            Text(quiz.question)
-                                .font(.title)
-                                .fontWeight(.semibold)
-                            Spacer()
-                        }.padding(.bottom) .padding(.top)
-                        
-                        VStack{
-                            ForEach(Array(quiz.choiceList.enumerated()), id: \.offset) { index, choice in
-                                HStack {
-                                    Text("\(index+1). \(choice)")
-                                        .font(.body)
-                                        .foregroundStyle(index + 1 == quiz.answer ? Color.init(uiColor: .systemIndigo) : Color.init(uiColor: .label))
-                                        .fontWeight(index + 1 == quiz.answer ? .bold : .regular)
-                                        //TODO: 유저가 고른 정답 == 원래 정답이면 green, 아니면 고른 정답은 red나 pink, 원래 정답은 indigo로 표현
-                                    Spacer()
+                    ForEach(viewModel.quizs, id: \.self) { quiz in
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 25)
+                                .foregroundColor(Color.indigo)
+                                .opacity(0.2)
+                                .padding()
+                        VStack (alignment: .leading){
+                            HStack {
+                                Text(quiz.question)
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                                Spacer()
+                            }.padding(.top) .padding(.horizontal)
+                            VStack (alignment: .center){
+                                ForEach(Array(quiz.choiceList.enumerated()), id: \.offset) { index, choice in
+                                    ZStack(alignment: .leading){
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .foregroundStyle(Color.white)
+                                            .opacity(0.9)
+                                            .shadow(radius: 3, x: 5, y: 5);                                                   VStack(alignment: .leading){
+                                                Text("\(index+1). \(choice)")
+                                                    .font(.body)
+                                                    .foregroundStyle(index + 1 == quiz.answer ? Color.init(uiColor: .systemGreen) : Color.init(uiColor: .label))
+                                                    .fontWeight(index + 1 == quiz.answer ? .bold : .regular)
+                                                    .lineLimit(nil)
+                                                    .fixedSize(horizontal: false, vertical: true)
+                                                    .padding(.horizontal)
+                                                    .padding(.vertical, 10)
+                                                if(index + 1 == quiz.answer){
+                                                    Text("\(quiz.reason)")
+                                                        .font(.body)
+                                                        .fontWeight(.semibold)
+                                                        .foregroundStyle(Color.init(uiColor: .systemGreen))
+                                                        .lineLimit(nil)
+                                                        .fixedSize(horizontal: false, vertical: true)
+                                                        .opacity(0.8)
+                                                        .padding(.bottom, 10)
+                                                        .padding(.horizontal)
+                                                }
+                                            }
+                                        //TODO: 유저가 고른 정답 == 원래 정답이면 green, 아니면 고른 정답은 red나 pink, 원래 정답은 green로 표현
+                                        Spacer()
+                                    }
                                 }
-                            }
-                        }
+                            } .padding()
+                            
+                        }.padding()
                     }
                 }
             }
-            .padding()
+            .padding(5)
         }
         ZStack{
             Button{
@@ -60,10 +86,10 @@ public struct QuestionResultView: View {
                 Text("뉴스 목록으로 돌아가기")
                     .frame(maxWidth: .infinity)
                     .controlSize(.large)
-                    .shadow(radius: 4, x: 0, y: 4)
             }
             .tint(.indigo)
             .buttonStyle(.borderedProminent)
+            .shadow(radius: 10, x: 4, y: 4)
         }.padding(.leading) .padding(.trailing)
         .onReceive(viewModel.$popToRoot) { popToRoot in
             if popToRoot {
