b429700d36d54544352b7a83b395bd9625253006
송영모 <mooyoung2309@gmail.com>
2023-10-17 13:47:19 +0900

    feat: cell을 탭뷰로 변경

--- Targets/D3N/Sources/Feature/Quiz/Main/QuizMainView.swift
+++ Targets/D3N/Sources/Feature/Quiz/Main/QuizMainView.swift
@@ -46,6 +46,7 @@ public struct QuizMainView: View {
                 )
             ) {
                 QuizListView(store: $0)
+                    .presentationDetents([.medium])
             }
             .toolbar(.hidden, for: .tabBar)
         }
